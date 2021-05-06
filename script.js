860fb2414b4947968c3e89e5caf5039c151ce461
Tulio <tuliotrindade99@gmail.com>
2021-05-06 18:56:45 -0300

    requisito 8 concluido

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let botao = document.querySelector('#criar-tarefa');
-let lista = document.querySelector('#lista-tarefas');
-let entrada = document.querySelector('#texto-tarefa');
-botao.addEventListener('click', function () {
-  let tarefa = document.createElement('li');
-  tarefa.id = 'tarefas';
-  tarefa.textContent = entrada.value;
-  lista.appendChild(tarefa);
-  entrada.value = '';
-});
-let corTarefa = document.querySelector('#lista-tarefas');
-corTarefa.addEventListener('click', function (evento) {
-  evento.target.style.backgroundColor = 'rgb(128, 128, 128)';
-});
+let botao=document.querySelector("#criar-tarefa")
+let lista=document.querySelector("#lista-tarefas")
+let entrada=document.querySelector("#texto-tarefa")
+let ntarefas=document.querySelectorAll("ol")
+let i=0;
+botao.addEventListener("click", function(){
+    let tarefa=document.createElement("li");
+    tarefa.className="tarefas"
+    tarefa.textContent=entrada.value;
+    lista.appendChild(tarefa);
+    entrada.value="";
+})
+let corTarefa=document.querySelector("#lista-tarefas");
+corTarefa.addEventListener("click", function(evento){
+    if(evento.target.className=="tarefas"){
+        if(document.querySelector("#target")!==null){
+            document.querySelector("#target").removeAttribute("id")
+        }
+    evento.target.id="target"
+}     
+})
