0ff343cc464d3514288dd24dc6cb28bc59d2af76
Tulio <tuliotrindade99@gmail.com>
2021-05-06 16:03:40 -0300

    requisito 7 concluido

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let botao=document.querySelector("#criar-tarefa")
-let lista=document.querySelector("#lista-tarefas")
-let entrada=document.querySelector("#texto-tarefa")
-let i=0;
-botao.addEventListener("click", function(){
-    let tarefa=document.createElement("li");
-    tarefa.id="tarefas"
-    console.log(entrada);
-    tarefa.textContent=entrada.value;
-    lista.appendChild(tarefa);
-    entrada.value="";
-
-})
+let botao = document.querySelector('#criar-tarefa');
+let lista = document.querySelector('#lista-tarefas');
+let entrada = document.querySelector('#texto-tarefa');
+botao.addEventListener('click', function () {
+  let tarefa = document.createElement('li');
+  tarefa.id = 'tarefas';
+  tarefa.textContent = entrada.value;
+  lista.appendChild(tarefa);
+  entrada.value = '';
+});
+let corTarefa = document.querySelector('#lista-tarefas');
+corTarefa.addEventListener('click', function (evento) {
+  evento.target.style.backgroundColor = 'rgb(128, 128, 128)';
+});
